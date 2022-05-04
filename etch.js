44d6943bb42a2b6a1b262e97cc740b8377eee0a3
EDiT0330 <ethan.tu246@gmail.com>
2022-05-04 15:36:16 -0700

    first couple files

---
+++ etch.js
@@ -0,0 +1,11 @@
+var cont = document.getElementById("container");
+
+
+function createGrid(){
+    for(let i = 0; i < 16; i++){
+        const container = document.createElement("div");
+        
+    }
+}
+
+window.addEventListener('load');
